b151f1e7d30be75edd62077daefb0044fd1e980f
jeffluz <jeffluz@gmail.com>
2016-03-27 15:26:41 -0300

    Implementation for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function cacheSolve receives a matrix and returns the inverse matrix
+## The function makeCacheMatrix calculates the inverse matrix or recovers the cached calculation
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  
+  # Storing the received matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # Returning the stored matrix
+  get <- function() x
+  
+  # Storing the received inverse matrix
+  setmtx <- function(minv) m <<- minv
+  
+  # Returning the stored inverse matrix
+  getmtx <- function() m
+  
+  list(set = set, get = get, setmtx = setmtx, getmtx = getmtx)
+  
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Recovering the possible stored inverse matrix
+  m <- x$getmtx()
+  
+  if(!is.null(m)) {
+    # Returning stored inverse matrix
+    return(m)
+  }
+  
+  # Calculating inverse matrix because there is no previous one
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmtx(m)
+  m
+  
 }
